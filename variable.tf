01fa560344a47137774751f8d5e45145b368aa46
root <root@ip-172-31-58-202.ec2.internal>
2020-01-25 03:22:49 +0000

    test

--- variable.tf
+++ variable.tf
@@ -3,8 +3,8 @@ variable "aws_creds" {
   description = "AWS credentials for terraform"
 
   default = {
-    access_key = "AKIAQ2GSFJYE6G75ZW5W"
-    secret_key       = "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"
+    access_key = ""
+    secret_key       = ""
     region   = "us-east-1"
   }
 }
